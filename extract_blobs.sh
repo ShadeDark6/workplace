9b3d473c6171b2c7ce88563a3883dae00c2f448d
ShadeDark6 <nizamfa6@gmail.com>
2025-12-11 20:57:38 +0000

    Adapt to Marble Device

--- extract_blobs.sh
+++ extract_blobs.sh
@@ -5,24 +5,52 @@ if [ ! -d "lineage" ]; then
     mkdir -p lineage
 fi
 
-# Step 3: Extract Blobs
+# Path to your dumped firmware (DumprX output)
+DUMP_PATH="../../../../DumprX/out"
 
-echo "Checking and cloning or updating Xiaomi peridot device tree..."
-if [ -d "lineage/device/xiaomi/peridot" ]; then
-    echo "Xiaomi Peridot device tree already exists, updating..."
-    cd lineage/device/xiaomi/peridot
+# ---------------------------------------------------------
+# 1. Clone/Update Xiaomi SM8450-common
+# ---------------------------------------------------------
+echo "Checking and cloning or updating Xiaomi SM8450-common..."
+COMMON_PATH="lineage/device/xiaomi/sm8450-common"
+
+if [ -d "$COMMON_PATH" ]; then
+    echo "Xiaomi SM8450-common already exists, updating..."
+    cd "$COMMON_PATH"
     git pull
     cd ../../../..
 else
-    git clone https://github.com/peridot-hyperos-2/device_xiaomi_peridot.git -b aosp-16 lineage/device/xiaomi/peridot
+    # Cloning pa-gr / vauxite
+    git clone https://github.com/pa-gr/android_device_xiaomi_sm8450-common.git -b vauxite "$COMMON_PATH"
 fi
 
-echo "Running setup-makefiles.sh in the peridot directory..."
-cd lineage/device/xiaomi/peridot
+# ---------------------------------------------------------
+# 2. Clone/Update Xiaomi Marble
+# ---------------------------------------------------------
+echo "Checking and cloning or updating Xiaomi Marble..."
+MARBLE_PATH="lineage/device/xiaomi/marble"
+
+if [ -d "$MARBLE_PATH" ]; then
+    echo "Xiaomi Marble already exists, updating..."
+    cd "$MARBLE_PATH"
+    git pull
+    cd ../../../..
+else
+    # Cloning pa-gr / vauxite
+    git clone https://github.com/pa-gr/android_device_xiaomi_marble.git -b vauxite "$MARBLE_PATH"
+fi
+
+# ---------------------------------------------------------
+# 3. Extraction (Triggers Common + Device)
+# ---------------------------------------------------------
+echo "Starting extraction process from Marble..."
+cd "$MARBLE_PATH"
+
+echo "Running setup-makefiles.sh..."
 bash setup-makefiles.sh
 
-echo "Running extract-files.sh using output from lineage/DumprX..."
-bash extract-files.sh ../../../DumprX/out
+echo "Running extract-files.sh..."
+# This will extract Marble blobs AND automatically trigger sm8450-common blobs
+bash extract-files.sh "$DUMP_PATH"
 
 echo "Blobs extracted successfully!"
-
